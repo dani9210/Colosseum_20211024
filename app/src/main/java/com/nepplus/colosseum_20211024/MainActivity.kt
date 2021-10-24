d5668cbeeca9e03a8a65b19abf0a467c710840ee
김다은 <softeun92@naver.com>
2021-10-24 12:12:14 +0900

    binding 활용 - 버튼 이벤트처리, 로그 출력

--- app/src/main/java/com/nepplus/colosseum_20211024/MainActivity.kt
+++ app/src/main/java/com/nepplus/colosseum_20211024/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nepplus.colosseum_20211024
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.nepplus.colosseum_20211024.databinding.ActivityMainBinding
 
@@ -14,5 +15,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binging =  DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binging.loginBtn.setOnClickListener {
+
+
+//          입력한 이메일 / 비번을 데이터 바인딩으로 가져오기.
+            val inputEmail = binging.emailEdt.text.toString()
+            val inputPw = binging.passwordEdt.text.toString()
+
+//          가져온 이메일 / 비번을 로그로 출력.
+
+            Log.d("입력이메일", inputEmail)
+            Log.d("입력비번",inputPw)
+
+
+
+        }
     }
 }
