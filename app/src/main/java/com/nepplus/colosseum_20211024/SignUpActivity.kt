80f35fc15c9d8a4c34b4c86d944047970d61dd9f
김다은 <softeun92@naver.com>
2021-10-31 10:23:31 +0900

    회원가입 화면 추가   - 데이터 바인딩 세팅

---
+++ app/src/main/java/com/nepplus/colosseum_20211024/SignUpActivity.kt
@@ -0,0 +1,17 @@
+package com.nepplus.colosseum_20211024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nepplus.colosseum_20211024.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+
+    lateinit var binding : ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
+    }
+}
