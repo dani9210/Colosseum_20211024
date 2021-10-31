69960ee348ee05cfeb1270f19e85e4740114f5d4
김다은 <softeun92@naver.com>
2021-10-31 14:08:30 +0900

    메인화면  새로 추가   - BaseActivity 상속   - 데이터바인딩 세팅

---
+++ app/src/main/java/com/nepplus/colosseum_20211024/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.nepplus.colosseum_20211024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nepplus.colosseum_20211024.databinding.ActivityLoginBinding
+import com.nepplus.colosseum_20211024.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity() {
+
+
+    lateinit var binding : ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
