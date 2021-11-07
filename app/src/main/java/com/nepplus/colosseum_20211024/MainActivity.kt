7ee08980d28cacfae80f9ca2107353d9dcf0e71c
김다은 <softeun92@naver.com>
2021-11-07 10:51:06 +0900

    메인 화면 정보를 서버에 요청하는 API추가   - 파싱작업 X

--- app/src/main/java/com/nepplus/colosseum_20211024/MainActivity.kt
+++ app/src/main/java/com/nepplus/colosseum_20211024/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.nepplus.colosseum_20211024.databinding.ActivityLoginBinding
 import com.nepplus.colosseum_20211024.databinding.ActivityMainBinding
+import com.nepplus.colosseum_20211024.datas.TopicData
 import com.nepplus.colosseum_20211024.utils.ServerUtil
 import org.json.JSONObject
 
@@ -12,6 +13,8 @@ class MainActivity : BaseActivity() {
 
 
     lateinit var binding: ActivityMainBinding
+
+    val mTopicList = ArrayList<TopicData>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +30,21 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        /v2/main_/main_info API가 토론 주제 목록을 내려줌.
+//        서버 호출 => 파싱해서, mTopicList를 채워주자.
+        getTopicListFromServer()
+
+
+
+    }
+
+
+
+    fun getTopicListFromServer() {
+
+        ServerUtil.getRequestMainInfo(mComtext, object : ServerUtil.JsonResponseHandler)
+
 
 
 
