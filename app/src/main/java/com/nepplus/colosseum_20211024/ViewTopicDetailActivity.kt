4e283583a17b3c70d9df444433b33188773f4e9a
김다은 <softeun92@naver.com>
2021-11-07 12:34:37 +0900

    토론주제 상세보기 추가

---
+++ app/src/main/java/com/nepplus/colosseum_20211024/ViewTopicDetailActivity.kt
@@ -0,0 +1,29 @@
+package com.nepplus.colosseum_20211024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nepplus.colosseum_20211024.databinding.ActivityViewTopicDetailBinding
+
+class ViewTopicDetailActivity : BaseActivity() {
+
+    lateinit var binding: ActivityViewTopicDetailBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_view_topic_detail)
+        setupEvents()
+        setValues()
+    }
+
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
