954f83054ddd0884585656122e486ba8ff5c3074
김다은 <softeun92@naver.com>
2021-11-07 12:16:59 +0900

    SplashActivity 로직 작성   - 저장된 토큰이 있는지 / 없는지에따라 다른 화면으로 이동   - 자동 로그인 구현

--- app/src/main/java/com/nepplus/colosseum_20211024/SplashActivity.kt
+++ app/src/main/java/com/nepplus/colosseum_20211024/SplashActivity.kt
@@ -1,9 +1,13 @@
 package com.nepplus.colosseum_20211024
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.databinding.DataBindingUtil
 import com.nepplus.colosseum_20211024.databinding.ActivitySplashBinding
+import com.nepplus.colosseum_20211024.utils.ContextUtil
 
 class SplashActivity : BaseActivity() {
 
@@ -25,6 +29,45 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        val myHandler = Handler(Looper.getMainLooper() )
+
+        myHandler.postDelayed( {
+
+//           자동 로그인을 해도 되는지 검사.
+//           1. 토큰값이 저장된게 있는가?
+//           2. (차후 작성)   그 토큰이 실제로 유효한가? =>  데이터를 받아올 수 잇는 토큰?
+
+            val myIntnt : Intent
+
+           if ( ContextUtil.getToken(mComtext) != "" ){
+
+
+               myIntnt = Intent(mComtext, MainActivity::class.java)
+
+           }
+//               저장된 토큰이 있다!
+//                -> 메인화면으로 넘어가자.
+
+           else{
+
+//               저장된 토큰이 "" 이다 =>  없다!
+//               -> 새로 로그인 해야함. -> 로그인 화면으로 넘어가자
+
+               myIntnt = Intent(mComtext, LoginActivity::class.java)
+
+
+           }
+
+
+            startActivity(myIntnt)
+
+
+        },   2500  )
+
+
+
+
 
     }
 }
