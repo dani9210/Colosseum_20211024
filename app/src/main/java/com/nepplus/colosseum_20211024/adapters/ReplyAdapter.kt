a3cd8cc9866091c73f15e0bf95f880a9853937e8
김다은 <softeun92@naver.com>
2021-11-07 14:16:44 +0900

    의견 어댑터 뼈대 작업   - 복붙활용

---
+++ app/src/main/java/com/nepplus/colosseum_20211024/adapters/ReplyAdapter.kt
@@ -0,0 +1,45 @@
+package com.nepplus.colosseum_20211024.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.nepplus.colosseum_20211024.R
+import com.nepplus.colosseum_20211024.datas.ReplyData
+import com.nepplus.colosseum_20211024.datas.TopicData
+
+class ReplyAdapter(
+    val mContext: Context,
+    resId: Int,
+    val mList: List<ReplyData>) : ArrayAdapter<ReplyData>(mContext,resId,mList) {
+
+
+        val mInflater = LayoutInflater.from(mContext)
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null)  {
+
+            tempRow = mInflater.inflate(R.layout.reply_list_item, null)
+
+        }
+
+        val row = tempRow!!
+
+        val data = mList[position]
+
+
+
+
+        return row
+
+
+    }
+
+
+
+}
