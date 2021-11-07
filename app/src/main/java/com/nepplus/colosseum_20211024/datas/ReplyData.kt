190f173447b729e6fc00d2962513d6b647a73cb2
김다은 <softeun92@naver.com>
2021-11-07 16:20:00 +0900

    댓글 상세 정보 파싱 준비   - 표현할 UI만 우선 작업

--- app/src/main/java/com/nepplus/colosseum_20211024/datas/ReplyData.kt
+++ app/src/main/java/com/nepplus/colosseum_20211024/datas/ReplyData.kt
@@ -2,6 +2,8 @@ package com.nepplus.colosseum_20211024.datas
 
 class ReplyData(var id : Int,var content : String) {
 
+//    var user : UserData 댓글 적은 사람 정도=> 하위 데이터로.
+
 
     constructor() : this(0,"내용없음")
 
