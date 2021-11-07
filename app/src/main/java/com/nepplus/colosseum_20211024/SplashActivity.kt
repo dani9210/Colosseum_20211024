5f9ba4c7e464446401e868e9b6292deb2d2804fd
김다은 <softeun92@naver.com>
2021-11-07 12:05:41 +0900

    SplashActivity 추가   - 데이터 확인 화면

---
+++ app/src/main/java/com/nepplus/colosseum_20211024/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.nepplus.colosseum_20211024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nepplus.colosseum_20211024.databinding.ActivitySplashBinding
+
+class SplashActivity : BaseActivity() {
+
+
+    lateinit var binding : ActivitySplashBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
